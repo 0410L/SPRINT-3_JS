9222198eb4bb3a27d3119cb94d418a120e101c02
0410L <oriol.busquet@gmail.com>
2022-08-30 23:18:10 +0200

    4.2

--- js/shop.js
+++ js/shop.js
@@ -116,6 +116,27 @@ function calculateTotal() {
 function generateCart() {
     // Using the "cartlist" array that contains all the items in the shopping cart, 
     // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
+    var entradaCesta = {};
+    cartList.forEach(item => {
+        if(cart.length >= 1) {
+            cart.forEach(lego => {
+                if(item.id == lego.id){}
+            
+            });
+        }
+        else{
+            entradaCesta ={
+                id: item.id,
+                name: item.name,
+                price: item.price,
+                type: item.type,
+                offer: item.offer,
+                quantity: 1,
+                totalPrice: item.price,
+            };
+
+        }
+    });
 }
 
 // Exercise 5
