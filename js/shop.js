1cef646bc07e663ec29ee9436f3265d59cce6ce7
0410L <oriol.busquet@gmail.com>
2022-09-01 19:10:43 +0200

    4.4

--- js/shop.js
+++ js/shop.js
@@ -116,6 +116,11 @@ function calculateTotal() {
 function generateCart() {
     // Using the "cartlist" array that contains all the items in the shopping cart, 
     // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
+ 
+
+    
+    
+    
     // var entradaDeCesta = {};
     // cartList.forEach(item => {  
         // if(cart.length >=1) {
